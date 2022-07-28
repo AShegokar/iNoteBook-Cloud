bbae249711615ce8c482f15b8b3651bcb8993885
Ajinkya Shegokar <ajinkyashegokar@Ajinkyas-MBP.lan>
2022-07-28 18:21:03 -0500

    adding data to database

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,28 @@
-const connectToMongo = require('./db');
-const express = require('express')
+const connectToMongo = require("./db");
+const express = require("express");
 
-connectToMongo()
+connectToMongo();
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.json());
+
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/notes", require("./routes/notes"));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/api/v1/login", (req, res) => {
+  res.send("Hello login!");
+});
+
+app.get("/api/v1/signUp", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port at http://localhost:${port}`)
-})
+  console.log(`Example app listening on port at http://localhost:${port}`);
+});
