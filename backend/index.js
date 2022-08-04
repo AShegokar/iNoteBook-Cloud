84b42107240b59a9fb188994885d3b04180bba45
Ajinkya Shegokar <ajinkyashegokar@Ajinkyas-MBP.lan>
2022-08-04 15:54:36 -0500

    add validator

--- backend/index.js
+++ backend/index.js
@@ -11,18 +11,6 @@ app.use(express.json());
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/notes", require("./routes/notes"));
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.get("/api/v1/login", (req, res) => {
-  res.send("Hello login!");
-});
-
-app.get("/api/v1/signUp", (req, res) => {
-  res.send("Hello World!");
-});
-
 app.listen(port, () => {
   console.log(`Example app listening on port at http://localhost:${port}`);
 });
